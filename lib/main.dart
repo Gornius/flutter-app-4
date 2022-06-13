23708529d9ceb8f050cbcb58abab7bd21a0d64c3
Rafał Cisek <rafal.cisek121@gmail.com>
2022-06-13 16:35:56 +0200

    code cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -19,64 +19,21 @@ class MyApp extends StatelessWidget {
           colorScheme: const ColorScheme.dark().copyWith(
         secondary: Colors.red,
       )),
-      home: const PeopleListViewPage(),
+      home: const PeopleListViewPage(
+        title: 'Testowy tytuł',
+      ),
     );
   }
 }
 
 class PeopleListViewPage extends StatelessWidget {
-  const PeopleListViewPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Maslo")),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const PeopleListViewPage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      appBar: AppBar(title: Text(title)),
     );
   }
 }
